1b48e1393524453a3750fefcc5fde48bd8f552ed
hit-JB <7034351669@gmail.com>
2021-07-25 11:46:35 +0800

    commit to temp

--- LetPain/src/Solution.java
+++ LetPain/src/Solution.java
@@ -3,12 +3,16 @@ import org.junit.Test;
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Solution sol = new Solution();
-        int[][] intervals = {{3,5},{12,15}};
-        int[][] insert = sol.insert(intervals,new int[]{6,6});
-        List<int[]> a = new ArrayList<>(Arrays.asList(insert));
-        a.forEach(e->System.out.println(e[0] + " " + e[1]));
+//        int[] nums = {536870912,0,534710168,330218644,142254206};
+//        int[][] queries =  {{558240772,1000000000},{307628050,1000000000},{3319300,1000000000},
+//                {2751604,683297522},{214004,404207941}};
+//        int[] ret = sol.maximizeXor(nums,queries);
+//        for(int e:ret)
+//            System.out.println(e);
+        int[][] nums = {{2,1},{3,4},{3,2}};
+        System.out.println(sol.restoreArray(nums));
     }
     public static void listForeach(ListNode listNode){
         while (listNode!=null) {
@@ -163,5 +167,447 @@ public class Solution {
             retArray[j] = ret.get(j);
         return retArray;
     }
+    public int findMaximumXOR(int[] nums) {
+        int max_size = 30;
+        int ret = 0;
+        for(int i=max_size;i>=0;i--){
+            Set<Integer> seen = new HashSet<>();
+            for(int num:nums){
+                seen.add(num>>i);
+            }
+            int x_hot = ret * 2+1;
+            boolean found = false;
+            for(int num:nums){
+                if (seen.contains(x_hot ^ num >> i)) {
+                    found = true;
+                    break;
+                }
+            }
+            if(found)
+                ret = x_hot;
+            else
+                ret = x_hot-1;
+        }
+        return ret;
+    }
+    public int findMaximumXORII(int[] nums){
+        NumNode node = new NumNode();
+        NumNode head = node;
+        int m =Integer.MIN_VALUE;
+        for(int e:nums){
+            m = Math.max(m,e);
+        }
+        int pow = (int) (Math.log(m) / Math.log(2));
+        for(int e:nums){
+            node = head;
+            for(int i=pow;i>=0;i--){
+                int hot = (e>>i) & 1;
+                if(node.next[hot]==null){
+                    node.next[hot] = new NumNode();
+                }
+                node = node.next[hot];
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for(int e:nums){
+            int sum = 0;
+            node = head;
+            for(int i=pow;i>=0;i--){
+                int pre = e>>i & 1;
+                if(node.next[1-pre]!=null) {
+                    sum = 2*sum+1;
+                    node = node.next[1 - pre];
+                }
+                else {
+                    node = node.next[pre];
+                    sum = 2 *sum;
+                }
+            }
+            max = Math.max(sum,max);
+        }
+        return max;
+    }
+    public int[] maximizeXor(int[] nums, int[][] queries) {
+        NumNode node = new NumNode();
+        NumNode head = node;
+        int m =Integer.MIN_VALUE;
+        for(int e:nums){
+            m = Math.max(m,e);
+        }
+        int pow = (int) (Math.log(m) / Math.log(2));
+        for(int e:nums){
+            node = head;
+            for(int i=pow;i>=0;i--){
+                int hot = (e>>i) & 1;
+                if(node.next[hot]==null){
+                    node.next[hot] = new NumNode();
+                }
+                node = node.next[hot];
+            }
+        }
+        int[] ret  = new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            int x_i = queries[i][0],m_i = queries[i][1];
+            int j = (int)Math.pow(2,pow);
+            node = head;
+            while(node != null && j>m_i){
+                j = j>>1;
+                node = node.next[0];
+            }
+            if(node==null){
+                ret[i] = -1;
+            }else{
+                int x_binary = (int)(Math.log(x_i)/Math.log(2));
+                int count=(int)(Math.log(j) / Math.log(2));
+                int num = 0;
+                for(int k=x_binary;k>count;k--)
+                    num += 1<<x_binary;
+                NumNode node_ = node;
+                while(node_!=null){
+                    if(node_.next[1]!=null)
+                        break;
+                    node_ = node_.next[0];
+                }
+                if(node_==null)
+                    ret[i] =-1;
+                else {
+                    int max = 0;
+                    while (count >= 0) {
+                        int e = (x_i >> count) & 1;
+                        if (node.next[1 - e] != null) {
+                            node = node.next[1 - e];
+                            max = 2 * max + 1;
+                        } else {
+                            node = node.next[e];
+                            max = 2 * max;
+                        }
+                        count--;
+                    }
+                    ret[i] = num + max;
+                }
+            }
+        }
+        return ret;
+    }
+    public int[] buildArray(int[] nums) {
+        int[] ret = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            ret[i] = nums[nums[i]];
+        }
+        return ret;
+    }
+    public int eliminateMaximum(int[] dist, int[] speed) {
+        List<Integer> time  = new ArrayList<>();
+        for(int i=0;i<dist.length;i++) {
+            if (dist[i] % speed[i] == 0)
+                time.add(dist[i] / speed[i]);
+            else
+                time.add(dist[i] / speed[i] + 1);
+        }
+        time.sort(Integer::compare);
+        for(int i=0;i<time.size();i++){
+            if(time.get(i)<=i)
+                return i;
+        }
+        return time.size();
+    }
+    public int countGoodNumbers(long n) {
+        int mod =(int) Math.pow(10,9) +7;
+        long odd,even;
+        odd = n/2;
+        even = n-odd;
+        return (int)((quickPow(4,odd,mod) *
+                quickPow(5,even,mod)) % mod);
 
+    }
+    public long quickPow(long base,long pow,int mod){
+        if (pow==1)
+            return base % mod;
+        if(pow==0)
+            return 1;
+        long n = quickPow(base,pow /2,mod);
+        if(pow % 2==0){
+           return (n*n) % mod;
+        }
+        return (n * n * base) % mod;
+    }
+    public int[][] rotateGrid(int[][] grid, int k) {
+        int m = grid.length,n = grid[0].length;
+        int[][] clockWise = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+        int[][] unClockWise = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+        int[][][] direction = new int[][][]{clockWise,unClockWise};
+        int i=0,j=0;
+        while(m>=2 && n>=2){
+            int nums = m * n;
+            int k_ = k % nums;
+            int rotate = 1;
+            if(k_ > nums / 2){
+                rotate = 0;
+                k_ = nums - k_;
+            }
+            int row = i,column = j;
+            int[][] start_rotate = direction[1-rotate];
+            int d=0;
+            List<Integer> stack = new ArrayList<>();
+            while(k_>=0){
+                stack.add(grid[row][column]);
+                if(row+start_rotate[d][0] <i || row +start_rotate[d][0] >= i+m ||
+                column + start_rotate[d][1] < j || column +start_rotate[d][1] >= j+n)
+                    d++;
+                row += start_rotate[d][0];column += start_rotate[d][1];
+                k_--;
+            }
+            stack.remove(0);
+            Queue<Integer> queue = new ArrayDeque<>();
+            while(!stack.isEmpty())
+                queue.add(stack.remove(stack.size()-1));
+            int[][] num_rotate = direction[rotate];
+            d=0;
+            row = i;column =j;
+            do{
+                queue.add(grid[row][column]);
+                grid[row][column] = queue.remove();
+                if(row+num_rotate[d][0] <i || row +num_rotate[d][0] >=i+m ||
+                        column + num_rotate[d][1] < j || column +num_rotate[d][1] >=j+n)
+                    d++;
+                row += num_rotate[d][0];column+=num_rotate[d][1];
+            }while(row!=i || column!=j);
+            i++;j++;
+            m -=2;n-=2;
+        }
+        return grid;
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode headA_copy = headA,headB_copy = headB;
+        while(headA != headB){
+            headA=headA==null?headB_copy:headA.next;
+            headB=headB==null?headA_copy:headB.next;
+        }
+        return headA;
+    }
+    public long wonderfulSubstrings(String word) {
+        Map<Character,Long> map = new HashMap<>();
+        Map<Long,Long> count = new HashMap<>();
+        count.put(0L, 1L);
+        for(char c='a';c<='j';c++){
+            map.put(c, 1L <<(c-'a'));
+        }
+        long ret = 0L;
+        long sum=0;
+        for(int i=0;i<word.length();i++){
+            char c = word.charAt(i);
+            sum = sum^map.get(c);
+            ret += count.getOrDefault(sum,0L);
+            for(int k=0;k<=9;k++)
+            {
+                ret += count.getOrDefault((1<<k) ^ sum,0L);
+            }
+            count.put(sum,count.getOrDefault(sum,0L)+1);
+        }
+        return ret;
+    }
+    public int waysToBuildRooms(int[] prevRoom) {
+        Map<Integer,Integer> construct = new HashMap<>();
+        for(int i=1;i<prevRoom.length;i++){
+            construct.put(prevRoom[i],i);
+        }
+        return 0;
+    }
+    public void dfs(Map<Integer,Set<Integer>> map,Map<Integer,Integer> construct,
+                    int[] count,int node){
+        if(construct.isEmpty()){
+            count[0]++;
+            return;
+        }
+
+    }
+    public Node copyRandomList(Node head) {
+        if(head==null)
+            return null;
+        Node node = new Node(head.val);
+        Node ret = node;
+        Map<Node,Node> pairNode = new HashMap<>();
+        Node start = head;
+        pairNode.put(head,node);
+        while(start!=null){
+            if(pairNode.containsKey(start.next)){
+                node.next = pairNode.get(start.next);
+            }else{
+                node.next = start.next==null?null:new Node(start.next.val);
+                pairNode.put(start.next,node.next);
+            }
+
+            if(pairNode.containsKey(start.random)){
+                node.random = pairNode.get(start.random);
+            }else{
+                node.random = start.random==null?null:new Node(start.random.val);
+                pairNode.put(start.random,node.random);
+            }
+
+            start = start.next;
+            node = node.next;
+        }
+        return ret;
+    }
+    public double largestSumOfAverages(int[] nums, int k) {
+        double[][] dp = new double[nums.length][k];
+        double[] sum = new double[nums.length];sum[0] = nums[0];
+        for(int i=1;i<nums.length;i++)
+            sum[i] = sum[i-1] +nums[i];
+        for(int i=0;i<nums.length;i++){
+            dp[i][0] = sum[i] / (i+1);
+            for(int j=1;j<=i && j<k;j++){
+                for(int p=0;p<i;p++) {
+                    if(p<j-1)
+                        continue;
+                    dp[i][j] = Math.max(dp[p][j - 1] + (sum[i] - sum[p]) / (i - p), dp[i][j]);
+                }
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
+    }
+    public int[][] imageSmoother(int[][] img) {
+        int [][] grid = new int[img.length][img[0].length];
+        int[][] direction = new int[][]{{0,0},{-1,-1},{-1,0},{-1,1},{0,1},{1,1},
+                {1,0},{1,-1},{0,-1}};
+        for(int i=0;i<img.length;i++){
+            for(int j=0;j<img[0].length;j++){
+                int sum=0,count=0;
+                for (int[] ints : direction) {
+                    int row = i + ints[0], column = j + ints[1];
+                    if (row < 0 || row >= img.length ||
+                            column < 0 || column >= img[0].length)
+                        continue;
+                    sum += img[row][column];
+                    count += 1;
+                }
+                grid[i][j] = sum / count;
+            }
+        }
+        return grid;
+    }
+    public boolean isCovered(int[][] ranges, int left, int right) {
+        List<int[]> list = new ArrayList<>(Arrays.asList(ranges));
+        list.sort(Comparator.comparingInt(e -> e[0]));
+        Stack<int[]> stack = new Stack<>();
+        List<Stack<int[]>> ret = new ArrayList<>();
+        for(int[] e:list){
+            int left_ = e[0],right_ = e[1];
+            if(stack.isEmpty()) {
+                stack.push(e);
+            }
+            else{
+                if(left_<=stack.peek()[1]+1 && right_>stack.peek()[1])
+                    stack.push(e);
+                else if(left_>stack.peek()[1]+1){
+                    Stack<int[]> temp = new Stack<>();
+                    temp.addAll(stack);
+                    ret.add(temp);
+                    stack.clear();
+                    stack.push(e);
+                }
+            }
+        }
+        System.out.println(stack);
+        ret.add(stack);
+        for(Stack<int[]> e:ret){
+            if(e.get(0)[0]<=left && e.peek()[1]>= right)
+                return true;
+        }
+        return false;
+    }
+    public long maxAlternatingSum(int[] nums) {
+        if(nums.length==1)
+            return nums[0];
+        int i=0;
+        long sum = 0;
+        while(i<nums.length-1){
+            while(i+1<nums.length && nums[i+1]>=nums[i])
+                i++;
+            sum+= nums[i];
+            while(i+1<nums.length && nums[i+1]<=nums[i])
+                i++;
+            if(i+1<nums.length) {
+                sum -= nums[i];
+            }
+        }
+        return sum;
+    }
+    public String maximumTime(String time) {
+        StringBuilder builder = new StringBuilder();
+        for(int i=0;i<5;i++){
+            char c = time.charAt(i);
+            if(c=='?'){
+                if(i==0)
+                {
+                    if(time.charAt(i+1)=='?' || time.charAt(i+1)-'0'<4)
+                        builder.append(2);
+                    else
+                        builder.append(1);
+                }else
+                if(i==1){
+                    if(builder.charAt(0)=='2')
+                        builder.append(3);
+                    else
+                        builder.append(9);
+                }else
+                    if(i==3)
+                        builder.append(5);
+                else
+                        builder.append(9);
+            }
+            else
+                builder.append(c);
+        }
+        return builder.toString();
+    }
+    public int maxProfit(int[] prices) {
+        int[] buy = new int[prices.length],sale = new int[prices.length];
+        buy[0] = -prices[0];
+        for(int i=1;i<prices.length;i++){
+            sale[i] = Math.max(buy[i-1]+prices[i],sale[i-1]);
+            buy[i] = Math.max(buy[i-1],i-2>=0?sale[i-2]-prices[i]:-prices[i]);
+        }
+        return sale[prices.length-1];
+    }
+    public int leastBricks(List<List<Integer>> wall) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (List<Integer> e : wall) {
+            int sum = 0;
+            for (int i = 0; i < e.size() - 1; i++) {
+                int width = e.get(i);
+                sum += width;
+                count.put(sum, count.getOrDefault(sum, 0) + 1);
+            }
+        }
+        int max = 0;
+        for (Map.Entry<Integer, Integer> e : count.entrySet()) {
+            max = Math.max(max, e.getValue());
+        }
+        return wall.size() - max;
+    }
+    public int[] restoreArray(int[][] adjacentPairs) {
+        Map<Integer,List<Integer>> map = new HashMap<>();
+        for(int[] e:adjacentPairs){
+            map.computeIfAbsent(e[0],k->new ArrayList<>());
+            map.get(e[0]).add(e[1]);
+            map.computeIfAbsent(e[1],k->new ArrayList<>());
+            map.get(e[1]).add(e[0]);
+        }
+        int[] ret = new int[adjacentPairs.length+1];
+        for(Map.Entry<Integer,List<Integer>> e:map.entrySet()){
+            if(e.getValue().size()==1){
+                ret[0] = e.getKey();
+                break;
+            }
+        }
+        int next ,e;
+        for(int i=1;i<ret.length;i++){
+            e = ret[i-1];
+            List<Integer> list = map.get(e);
+            next = list.size()==1 || list.get(0)!= ret[i-2]?list.get(0):list.get(1);
+            ret[i] = next;
+        }
+        return ret;
+    }
 }
